5387e00e568b20671f4a94e0afd996736d693efe
Nate-yu <2242936124@qq.com>
2025-03-18 21:30:21 +0800

    注入

--- Code/ssm-parent/spring-01-ioc/src/main/java/com/example/spring/ioc/bean/Person.java
+++ Code/ssm-parent/spring-01-ioc/src/main/java/com/example/spring/ioc/bean/Person.java
@@ -1,10 +1,19 @@
 package com.example.spring.ioc.bean;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 
 @Data
 public class Person {
+    @Value("阿彬")
     private String name;
+    @Value("${person.age}")
     private int age;
+    @Value("男")
     private String gender;
+    /**
+     * 1. @Value("字面值")：直接赋值
+     * 2. @Value("${key}")：从配置文件中获取值
+     * 3. @Value("#{bean.属性}")：Spring表达式语言，从容器中获取值
+     */
 }
--- Code/ssm-parent/spring-01-ioc/src/main/java/com/example/spring/ioc/controller/UserController.java
+++ Code/ssm-parent/spring-01-ioc/src/main/java/com/example/spring/ioc/controller/UserController.java
@@ -1,7 +1,29 @@
 package com.example.spring.ioc.controller;
 
+import com.example.spring.ioc.dao.UserDao;
+import com.example.spring.ioc.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import jakarta.annotation.Resource;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Resource
+    private UserDao userDao;
+
+    /**
+     * @Autowired 和 @Resource 的区别
+     * 1. @Autowired 是 Spring 提供的注解，@Resource 是 Java 提供的注解，@Resource具有更强的通用性
+     * 2. @Autowired 是按照类型注入，@Resource 是按照名称注入
+     * 3. @Autowired 可以设置 required 属性，@Resource 不能设置 required 属性
+     * 4. @Autowired 可以设置 primary 属性，@Resource 不能设置 primary 属性
+     * 5. @Autowired 可以设置 qualifier 属性，@Resource 不能设置 qualifier 属性
+     * 6. @Autowired 可以设置 autowire 属性，@Resource 不能设置 autowire 属性
+     * 7. @Autowired 可以设置 value 属性，@Resource 不能设置 value 属性
+     */
+
 }
